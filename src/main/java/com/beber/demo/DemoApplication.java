729edfe3bc143b10874f8160e032c45c4d1d8000
bertrand <bertrand.chevallereau@gmail.com>
2020-01-29 07:53:17 +0100

    init

---
+++ src/main/java/com/beber/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.beber.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/beber/demo/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.beber.demo.hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Coucou ma chérie!";
+    }
+
+}
