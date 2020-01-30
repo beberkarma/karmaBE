b184be30cd8b700479886083021ef1f2da877bcf
bertrand <bertrand.chevallereau@gmail.com>
2020-01-30 08:27:36 +0100

    mise en place premier endpoint

--- src/main/java/com/beber/demo/hello/controller/PersonController.java
+++ src/main/java/com/beber/demo/hello/controller/PersonController.java
@@ -1,7 +1,24 @@
 package com.beber.demo.hello.controller;
 
+import com.beber.demo.hello.model.Person;
+import com.beber.demo.hello.repository.PersonRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PersonController {
+
+    private final PersonRepository personRepository;
+
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @GetMapping("/person")
+    List<Person> all() {
+        return personRepository.findAll();
+    }
+
 }
