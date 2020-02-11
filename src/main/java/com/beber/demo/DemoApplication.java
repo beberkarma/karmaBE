f2b0740ca92a5831f3eddbf5e2828e416b3fbd23
bertrand <bertrand.chevallereau@gmail.com>
2020-02-11 14:08:59 +0100

    configuration des CORS > all access origin dans un premier temps

--- src/main/java/com/beber/demo/DemoApplication.java
+++ src/main/java/com/beber/demo/DemoApplication.java
@@ -2,12 +2,27 @@ package com.beber.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                // TODO Attention: Autoriser n'importe quel site à accéder à unel'API est une mauvaise idée
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
